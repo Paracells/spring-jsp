9cd922710e8ebbf3fcfa5545eeb8dcc98f5f1e78
Paracells <enoalife@hotmail.com>
2020-04-30 21:55:40 +0300

    @InitBinder validation to lastName

--- src/main/java/org/spring/demo/demoapp/controllers/CustomerController.java
+++ src/main/java/org/spring/demo/demoapp/controllers/CustomerController.java
@@ -1,9 +1,12 @@
 package org.spring.demo.demoapp.controllers;
 
 import org.spring.demo.demoapp.customers.Customer;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,6 +15,16 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // add an initbinder ... to convert trim input strings
+    // remove leading and trailing whitspace
+    // resolve issue for our validation
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model model) {
